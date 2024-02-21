90bd4ac454cb7d67e934128517c01ce7f487f8b3
kirubel-web <kirubelalemu119@gmail.com>
2024-02-21 20:23:14 +0300

    fix;

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+""" __init__ magic method for models directory.
+
+-> If the environmental variable 'HBNB_TYPE_STORAGE' is set to 'db',
+   instantiates a database storage engine (DBStorage).
+-> Otherwise, instantiates a file storage engine (FileStorage).
+"""
 from os import getenv
 
 
